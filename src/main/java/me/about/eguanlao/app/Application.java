126b6c15a921ed782682cff3e2789eb23fad54cd
Erick Guanlao <ericguanlao@gmail.com>
2016-10-10 07:25:59 +1100

    First commit

---
+++ src/main/java/me/about/eguanlao/app/Application.java
@@ -0,0 +1,14 @@
+package me.about.eguanlao.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+
+  public static void main(final String[] args) {
+    SpringApplication.run(Application.class, args);
+  }
+
+}
